ab06244e22829eb5260419f3b1b65bcffc2840b3
TheKuroEver <mariusztang@outlook.com>
2023-02-16 22:28:46 +0000

    Update ColouredButton.cs

--- Assets/Samuel Says Scripts/ColouredButton.cs
+++ Assets/Samuel Says Scripts/ColouredButton.cs
@@ -3,37 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ColouredButton : MonoBehaviour {
-	[SerializeField] private MeshRenderer _buttonCover;
-	[SerializeField] private MeshRenderer _buttonBacking;
-	[SerializeField] private Light _light;
+    [SerializeField] private MeshRenderer _buttonCover;
+    [SerializeField] private MeshRenderer _buttonBacking;
+    [SerializeField] private Light _light;
 
-	private SamColour _colour;
+    public SamColour Colour { get;
+        private set;
+    }
+    public bool Lit {
+        set {
+            _light.enabled = value;
+        }
+    }
 
-	public SamColour Colour { get { return _colour; } }
-	public bool Lit { set { _light.enabled = value; } }
+    void Awake() {
+        Color[] buttonColourList = new Color[] {
+            new Color(88f / 255f, 0, 0, 0.75f),
+            new Color(88f / 255f, 88f / 255f, 0, 0.75f),
+            new Color(0, 88f / 255f, 0, 0.75f),
+            new Color(0, 0, 88f / 255f, 0.75f)
+        };
+        Color[] lightColourList = new Color[] {
+            Color.red,
+            Color.yellow,
+            Color.green,
+            Color.blue
+        };
+        int colourIndex = "ABCD".IndexOf(transform.name);
+        Color buttonColour = buttonColourList[colourIndex];
+        Color lightColour = lightColourList[colourIndex];
 
-	void Awake() {
-		Color[] buttonColourList = new Color[] {
-			new Color(88f / 255f, 0, 0, 0.75f),
-			new Color(88f / 255f, 88f / 255f, 0, 0.75f),
-			new Color(0, 88f / 255f, 0, 0.75f),
-			new Color(0, 0, 88f / 255f, 0.75f)
-		};
-		Color[] lightColourList = new Color[] {
-			Color.red,
-			Color.yellow,
-			Color.green,
-			Color.blue
-		};
-		int colourIndex = "ABCD".IndexOf(transform.name);
-		Color buttonColour = buttonColourList[colourIndex];
-		Color lightColour = lightColourList[colourIndex];	
+        _buttonCover.material.color = buttonColour;
+        _buttonBacking.material.color = buttonColour;
+        _light.color = lightColour;
+        Colour = (SamColour)colourIndex;
 
-		_buttonCover.material.color = buttonColour;
-		_buttonBacking.material.color = buttonColour;
-		_light.color = lightColour;
-		_colour = (SamColour)colourIndex;
-
-		_light.enabled = false;
-	}
+        _light.enabled = false;
+    }
 }
