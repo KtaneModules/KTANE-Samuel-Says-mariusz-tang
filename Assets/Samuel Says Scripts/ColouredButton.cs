844ece7cb8da05579eb2efb4d51fceae16d3ca5d
TheKuroEver <mariusztang@outlook.com>
2023-02-16 18:55:46 +0000

    We do a bit of formatting

--- Assets/Samuel Says Scripts/ColouredButton.cs
+++ Assets/Samuel Says Scripts/ColouredButton.cs
@@ -3,13 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ColouredButton : MonoBehaviour {
-	private readonly Color _red = new Color(88f / 255f, 0, 0, 0.75f);
-	private readonly Color _yellow = new Color(88f / 255f, 88f / 255f, 0, 0.75f);
-	private readonly Color _green = new Color(0, 88f / 255f, 0, 0.75f);
-	private readonly Color _blue = new Color(0, 0, 88f / 255f, 0.75f);
-
 	[SerializeField] private MeshRenderer _buttonCover;
-	[SerializeField] private MeshRenderer _backing;
+	[SerializeField] private MeshRenderer _buttonBacking;
 	[SerializeField] private Light _light;
 
 	private SamColour _colour;
@@ -18,29 +13,27 @@ public class ColouredButton : MonoBehaviour {
 	public bool Lit { set { _light.enabled = value; } }
 
 	void Awake() {
-		_light.enabled = false;
-		_buttonCover.transform.localPosition = new Vector3();
+		Color[] buttonColourList = new Color[] {
+			new Color(88f / 255f, 0, 0, 0.75f),
+			new Color(88f / 255f, 88f / 255f, 0, 0.75f),
+			new Color(0, 88f / 255f, 0, 0.75f),
+			new Color(0, 0, 88f / 255f, 0.75f)
+		};
+		Color[] lightColourList = new Color[] {
+			Color.red,
+			Color.yellow,
+			Color.green,
+			Color.blue
+		};
+		int colourIndex = "ABCD".IndexOf(transform.name);
+		Color buttonColour = buttonColourList[colourIndex];
+		Color lightColour = lightColourList[colourIndex];	
 
-		if (transform.name == "A") {
-			_buttonCover.material.color = _red;
-			_backing.material.color = _red;
-			_light.color = Color.red;
-			_colour = SamColour.Red;
-        } else if (transform.name == "B") {
-			_buttonCover.material.color = _yellow;
-			_backing.material.color = _yellow;
-			_light.color = Color.yellow;
-			_colour = SamColour.Yellow;
-		} else if (transform.name == "C") {
-			_buttonCover.material.color = _green;
-			_backing.material.color = _green;
-			_light.color = Color.green;
-			_colour = SamColour.Green;
-		} else if (transform.name == "D") {
-			_buttonCover.material.color = _blue;
-			_backing.material.color = _blue;
-			_light.color = Color.blue;
-			_colour = SamColour.Blue;
-		}
+		_buttonCover.material.color = buttonColour;
+		_buttonBacking.material.color = buttonColour;
+		_light.color = lightColour;
+		_colour = (SamColour)colourIndex;
+
+		_light.enabled = false;
 	}
 }
