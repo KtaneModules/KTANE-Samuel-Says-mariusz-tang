97a0323caa2e7d83e53d3937692e21a50a52cc29
TheKuroEver <mariusztang@outlook.com>
2023-02-16 02:50:58 +0000

    Added working lights!

---
+++ Assets/Samuel Says Scripts/ColouredButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColouredButton : MonoBehaviour {
+
+	[SerializeField] private Light _light;
+
+	public bool Lit { set { _light.enabled = value; } }
+
+	void Awake() {
+		_light.enabled = false;
+    }
+}
--- Assets/Samuel Says Scripts/SamuelSaysModule.cs
+++ Assets/Samuel Says Scripts/SamuelSaysModule.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class SamuelSaysModule : MonoBehaviour {
 
+	public ColouredButton[] Buttons;
+
 	// Use this for initialization
 	void Start () {
-		
+		foreach (ColouredButton button in Buttons) {
+			button.GetComponent<KMSelectable>().OnInteract += delegate () { button.Lit = true; return false; };
+			button.GetComponent<KMSelectable>().OnInteractEnded += delegate () { button.Lit = false; };
+		}
 	}
 	
 	// Update is called once per frame
